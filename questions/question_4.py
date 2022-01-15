43a4ce236f9810953090a5641f3f240f93f053fd
JoaoZati <joaozati@gmail.com>
2022-01-15 12:16:49 -0300

    Part 3: Start Question 3 and question 4.

---
+++ questions/question_4.py
@@ -0,0 +1,7 @@
+"""
+4) Write an efficient method that tells us whether or not an input string brackets ("{", "}",
+"(", ")", "[", "]") opened and closed properly. Example: “{[]}” => true, “{(])}” => false,
+“{([)]}” => false
+
+
+"""
