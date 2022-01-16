41e401fc38b87d96dab4a705dda840cba110add4
JoaoZati <joaozati@gmail.com>
2022-01-16 14:10:46 -0300

    Part 4:Question 4 done.

--- questions/question_4.py
+++ questions/question_4.py
@@ -3,5 +3,45 @@
 "(", ")", "[", "]") opened and closed properly. Example: “{[]}” => true, “{(])}” => false,
 “{([)]}” => false
 
-
+>>> properly_brackets("{[]}")
+True
+>>> properly_brackets("{[Hello Word],[Other Word]}")
+True
+>>> properly_brackets("[(){}")
+False
+>>> properly_brackets("{(])}")
+False
+>>> properly_brackets("{([)]}")
+False
 """
+
+
+def properly_brackets(string):
+    if not isinstance(string, str):
+        raise ValueError('The input must be a string')
+
+    brackets_open = {'{', '(', '['}
+    brackets_close = {'}', ')', ']'}
+
+    dict_brackets = {
+        '{': '}',
+        '(': ')',
+        '[': ']',
+    }
+
+    open_bracket = ''
+    for letter in string:
+        if letter in brackets_open:
+            open_bracket += letter
+            continue
+
+        if letter in brackets_close:
+            if letter == dict_brackets[open_bracket[-1]]:
+                open_bracket = open_bracket[:-1]
+                continue
+            return False
+
+    if open_bracket:
+        return False
+
+    return True
